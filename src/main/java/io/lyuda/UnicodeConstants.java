d8feb87d96290428d02f0470a7bc816af37f0714
null <NullRSJ@users.noreply.github.com>
2023-02-05 18:51:55 -0800

    Add constants class for displaying unicode representation of Cards

---
+++ src/main/java/io/lyuda/UnicodeConstants.java
@@ -0,0 +1,45 @@
+package io.lyuda;
+
+/**
+ * The UnicodeConstants class serves as a container for Unicode representations of playing cards,
+ * suits, and ranks.
+ */
+public class UnicodeConstants {
+
+    /** Unicode representation of the heart suit */
+    public static final String HEART = "\u2665";
+    /** Unicode representation of the diamond suit */
+    public static final String DIAMOND = "\u2666";
+    /** Unicode representation of the spade suit */
+    public static final String SPADE = "\u2660";
+    /** Unicode representation of the club suit */
+    public static final String CLUB = "\u2663";
+
+    /** Unicode representation of the Ace rank */
+    public static final String ACE = "\uD83C\uDCA1";
+    /** Unicode representation of the 2 rank */
+    public static final String TWO = "\uD83C\uDCA2";
+    /** Unicode representation of the 3 rank */
+    public static final String THREE = "\uD83C\uDCA3";
+    /** Unicode representation of the 4 rank */
+    public static final String FOUR = "\uD83C\uDCA4";
+    /** Unicode representation of the 5 rank */
+    public static final String FIVE = "\uD83C\uDCA5";
+    /** Unicode representation of the 6 rank */
+    public static final String SIX = "\uD83C\uDCA6";
+    /** Unicode representation of the 7 rank */
+    public static final String SEVEN = "\uD83C\uDCA7";
+    /** Unicode representation of the 8 rank */
+    public static final String EIGHT = "\uD83C\uDCA8";
+    /** Unicode representation of the 9 rank */
+    public static final String NINE = "\uD83C\uDCA9";
+    /** Unicode representation of the 10 rank */
+    public static final String TEN = "\uD83C\uDCAA";
+    /** Unicode representation of the Jack rank */
+    public static final String JACK = "\uD83C\uDCAB";
+    /** Unicode representation of the Queen rank */
+    public static final String QUEEN = "\uD83C\uDCAD";
+    /** Unicode representation of the King rank */
+    public static final String KING = "\uD83C\uDCAE";
+}
+
