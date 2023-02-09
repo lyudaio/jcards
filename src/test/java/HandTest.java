cc991c02bd011b6ef1f8a6dad28420e817125d10
lyudaio <lyduaio@users.noreply.github.com>
2023-02-08 19:49:37 -0800

    fix HandTest incorrect package declaration

--- src/test/java/HandTest.java
+++ src/test/java/HandTest.java
@@ -1,5 +1,6 @@
-package io.lyuda.jcards;
 
+import io.lyuda.jcards.Card;
+import io.lyuda.jcards.Hand;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
