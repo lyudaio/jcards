e4f1d5c5c94430440d4ea75c7bb0867cf2af3186
lyudaio <lyduaio@users.noreply.github.com>
2023-02-08 20:00:33 -0800

    Update javadocs to 0.0.2

--- docs/package-search-index.js
+++ docs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"io.lyuda.jcards"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"io.lyuda.jcards"},{"l":"io.lyuda.jcards.game"}];updateSearchResults();
